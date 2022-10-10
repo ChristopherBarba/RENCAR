049a4586e664ec7636114a57a1505515440deb46
ChristopherBarba <barbacamarena@gmail.com>
2022-10-10 13:04:04 -0500

    Se añadio ruta del index

--- servidor.js
+++ servidor.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   //res.end('Hello World!\n');
-  res.sendFile('index.html');
+  res.sendFile(path.resolve(__dirname,'index.html'))
 });
 
 server.listen(port, hostname, () => {
