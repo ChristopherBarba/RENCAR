c43a77e1c7e545c427896ce4a6a2b9e598e40a2a
ChristopherBarba <barbacamarena@gmail.com>
2022-10-10 13:07:21 -0500

    Se añadio ruta del index

--- servidor.js
+++ servidor.js
@@ -2,13 +2,26 @@ const http = require('http');
 
 const hostname = '74.208.159.121';
 const port = 443;
+const path = require('path');
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   //res.end('Hello World!\n');
-  res.sendFile(path.resolve(__dirname,'index.html'))
+  //res.sendFile(path.resolve(__dirname,'index.html'))
 });
+
+const app = server;
+
+app.use('/', require('./router'));
+
+app.get('/', (req, res) => {
+    //document.write(svg)
+
+res.sendFile(path.resolve(__dirname,'index.html'))
+res.send(z)
+
+})
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
