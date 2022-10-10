05339040f31055b371f159d445d0a1f44bfe6834
ChristopherBarba <barbacamarena@gmail.com>
2022-10-10 13:28:48 -0500

    Se añadio ruta del index

--- servidor.js
+++ servidor.js
@@ -7,17 +7,14 @@ const fs = require('fs').promises;
 const hostname = '74.208.159.121';
 const port = 443;
 
-const HTML_CONTENT_TYPE = 'text/html'
+
 
 const server = http.createServer((req, res) => {
-  //res.statusCode = 200;
-  //res.setHeader('Content-Type', 'text/plain');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
   //res.end('Hello World!\n');
-  //res.sendFile(path.resolve(__dirname,'index.html'))
-    // escribimos en la respuesta el status code de 200 y el content type que necesitamos
-    res.writeHead(200, { 'Content-Type': HTML_CONTENT_TYPE })
-    // leemos el fichero index.html y su contenido lo redirigimos a la respuesta
-    createReadStream('index.html').pipe(res)
+  res.sendFile(path.resolve(__dirname,'index.html'));
+
 
 });
 
