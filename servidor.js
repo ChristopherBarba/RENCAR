1b63797d432c5d79ffe1d1cfca1a73b5b8ff9f96
ChristopherBarba <barbacamarena@gmail.com>
2022-10-12 12:59:12 -0500

    Se cambio de servidor HTTP a EXPRESS

--- servidor.js
+++ servidor.js
@@ -1,17 +1,17 @@
-const http = require('http');
+/*const http = require('http');
 const express = require('express');
 const path = require('path');
 const app = express();
 const fs = require('fs').promises;
 
-const hostname = '74.208.159.121';
-//const hostname = 'localhost';
+//const hostname = '74.208.159.121';
+const hostname = 'localhost';
 const port = 443;
 
 //4 -seteamos el directorio de assets
-app.use('/resources',express.static('public'));
-app.use('/resources', express.static(__dirname + '/public'));
-
+//app.use('/resources',express.static('public'));
+//app.use('/resources', express.static(__dirname + '/public'));
+app.use(express.static('css'))
 //5 - Establecemos el motor de plantillas
 app.set('view engine','ejs');
 
@@ -40,3 +40,18 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+*/
+
+var express = require('express');
+var app = express();
+
+const hostname = 'localhost';
+const port = 443;
+
+app.get('/', function(req, res) {
+  res.send('Hola Mundo puñe!');
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
